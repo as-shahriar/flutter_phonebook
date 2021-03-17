7406fdbb64a7e24ced45bb1012eec008cde7e2e5
Asf Shahriar <sma.shahriar@asthait.com>
2021-03-17 14:03:02 +0600

    minor changes

--- lib/core/scoped_models/home_model.dart
+++ lib/core/scoped_models/home_model.dart
@@ -1,3 +1,4 @@
+import 'package:phonebook/Services/utils.dart';
 import 'package:phonebook/core/models/contact.dart';
 import 'package:phonebook/core/services/storage_service.dart';
 import 'package:phonebook/enums/view_state.dart';
@@ -8,34 +9,16 @@ import 'base_model.dart';
 
 class HomeModel extends BaseModel {
   StorageService storageService = locator<StorageService>();
-  String title = "HomeModel";
   DatabaseHelper _databaseHelper = DatabaseHelper.instance;
   List<BaseContact> contacts = [];
 
   void getContacts(Future<int> id) async {
     int userId = await id;
     List<Map> data = await _databaseHelper.fetchContactsByUser(userId);
-    setTitle("setter");
     for (var contact in data) {
       contacts.add(BaseContact.contact(new Contact.fromMap(contact)));
     }
-    print("Contacts");
-    print(contacts);
     notifyListeners();
-  }
-
-  Future<bool> saveData() async {
-    setState(ViewState.Busy);
-    setTitle("Saving Data");
-    await storageService.saveData();
-    setTitle("Data Saved");
-    setState(ViewState.Retrieved);
-
-    return true;
-  }
-
-  void setTitle(String value) {
-    title = value;
   }
 
   void addContact(Contact contact) {
--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:phonebook/Services/SharedPref.dart';
 import 'package:phonebook/Services/utils.dart';
+import 'package:phonebook/core/models/contact.dart';
 import 'package:phonebook/core/scoped_models/home_model.dart';
 import 'package:phonebook/enums/view_state.dart';
 import 'package:phonebook/themeConfig.dart';
@@ -12,10 +13,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../../service_locator.dart';
 import 'login_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   final SharedPrefs sharedPrefs = locator<SharedPrefs>();
+  List<Contact> contacts = [];
   @override
   Widget build(BuildContext context) {
+    var myAppModel = locator<HomeModel>();
+    myAppModel.getContacts(Utils.getUserID());
     return BaseView<HomeModel>(
       builder: (context, child, model) => Scaffold(
         appBar: AppBar(
@@ -42,25 +51,42 @@ class HomeView extends StatelessWidget {
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              InputField(
-                validationHandler: emailValidator,
-                onSaveHandler: ((value) => print(value)),
-                hintText: 'Search',
-              )
-            ],
-          ),
+        body: ListView.builder(
+          itemCount: model.contacts.length,
+          itemBuilder: (context, index) {
+            return Card(
+                child: ListTile(
+              title: Text('${index}'),
+            ));
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
+            print("Number of contact ${myAppModel.contacts.length}");
             Navigator.of(context).pushNamed('addContact');
           },
           child: Icon(Icons.add),
           backgroundColor: primaryColor,
         ),
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    var myAppModel = locator<HomeModel>();
+    print(myAppModel.contacts[index].contact.name);
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(myAppModel.contacts[index].contact.name ?? '',
+              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)),
+          Text(myAppModel.contacts[index].contact.address ?? '',
+              style: TextStyle(fontSize: 16.0)),
+          // Text(
+          //   contacts[index].phone ?? '',
+          //   style: TextStyle(fontSize: 18.0)),
+        ],
       ),
     );
   }
